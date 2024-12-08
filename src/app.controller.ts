694b2542d11ea96c8f87cddd336908def157e034
Au=Or[79]² <assanisaidelamani@gmail.com>
2024-12-08 02:46:08 +0100

    [EDIT] - init auth service as nest  TCP microservice

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern } from "@nestjs/microservices";
+import { of } from "rxjs";
+import { delay } from "rxjs/operators";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern({ cmd: "ping" })
+  ping(_: any) {
+    return of("pong").pipe(delay(1000));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: process.env.HOST,
+        port: parseInt(process.env.AUTHSERVICEPORT, 10), 
+      },
+    },
+  );
+  await app.listen();
+  console.log(`Auth service is listening on TCP ${process.env.HOST}:${parseInt(process.env.AUTHSERVICEPORT)}`);
 }
 bootstrap();
